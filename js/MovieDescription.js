4e0183baa23c48ef79943223fe96d9f83fdec417
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-04-27 07:44:25 +0200

    update

--- js/MovieDescription.js
+++ js/MovieDescription.js
@@ -1,9 +1,12 @@
-var MovieDescription = React.createClass ( {
+var MovieDescription = React.createClass({
 	propTypes: {
-    desc: React.PropTypes.string.isRequired,
-  },
-	render: function () {
-		return React.createElement('p', {},this.props.desc);
-		React.createElement('p', {},this.props.bohaters);
-}});
-
+		desc: React.PropTypes.string.isRequired,
+	},
+	render: function() {
+		 return React.createElement('div', {},
+            React.createElement('p', {}, this.props.desc),
+            React.createElement('p', {}, this.props.bohaters),
+            React.createElement('img', { src: this.props.images })
+        );
+	}
+});
